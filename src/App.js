ebc823a18a3da695d46c5ade6ada3dd161a41d61
Rilso <53212931+chimmykk@users.noreply.github.com>
2023-08-21 18:02:14 +0530

    Update App.js
    mywork

--- src/App.js
+++ src/App.js
@@ -7,19 +7,85 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+ 
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/chimmykk/gmbotapi"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         GM Extension API
+        </a>
+        
+        <a
+          className="App-link"
+          href="https://github.com/chimmykk/Awoo-Extension"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         Awoo Extension
+        </a>
+        <a
+          className="App-link"
+          href="https://github.com/chimmykk/gmExtension-site-for-dogePoundNFT"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         Awoo Extension Site Page
+        </a>
+        <a
+          className="App-link"
+          href="https://github.com/chimmykk/DogePound_Proxy_Extension/tree/main"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         Proxy Wallet-With Improved Auth Mech
+        </a>
+        <a
+          className="App-link"
+          href="https://github.com/chimmykk/twitter-play"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         Twitter Auto-Reply(AI Doge)
+        </a>
+        <a
+          className="App-link"
+          href="https://github.com/chimmykk/DogeConnect"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        DogeConnect
+        </a>
+        <a
+          className="App-link"
+          href="https://github.com/chimmykk/Train-Face-Doge"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        AI Doge Detection(Face/SWAP/Replace)
+        </a>
+        <a
+          className="App-link"
+          href="https://github.com/chimmykk/MemeGenerator"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+      Meme-Generator(On Process awaiting art)
+        </a>
+        <a
+          className="App-link"
+          href="https://github.com/chimmykk/MemeGenerator"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+      Twitch & News Reading script
         </a>
       </header>
     </div>
   );
 }
+
 
 export default App;
